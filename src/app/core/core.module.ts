3851bfa9cb45eab8eedd9b7e28b706babe9a8052
Matheus Ravanello Mokwa <matheus@tds.com.br>
2022-09-01 11:29:51 -0300

    add locale settings

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,28 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+import { CommonModule, registerLocaleData } from '@angular/common';
 import { AUTH_TOKEN_INTERCEPTOR } from './interceptors/auth-token.interceptor';
 import { HttpClientModule } from '@angular/common/http';
 import { API_SERVICE_INTERCEPTOR } from './interceptors/api-service-interceptor';
+
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt-BR');
+
+export const LOCALE_SETTIGNS = [
+  {
+    provide: LOCALE_ID,
+    useValue: 'pt-BR',
+  },
+  {
+    provide: DEFAULT_CURRENCY_CODE,
+    useValue: 'BRL',
+  },
+];
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, HttpClientModule],
   exports: [HttpClientModule],
-  providers: [AUTH_TOKEN_INTERCEPTOR, API_SERVICE_INTERCEPTOR],
+  providers: [AUTH_TOKEN_INTERCEPTOR, API_SERVICE_INTERCEPTOR, LOCALE_SETTIGNS],
 })
 export class CoreModule {}
