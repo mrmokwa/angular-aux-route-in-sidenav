cbd37d56d9d35780336b8f4272d9295b899f45e0
Matheus Ravanello Mokwa <matheus@tds.com.br>
2022-09-09 16:03:44 -0300

    ajustar interface do item

--- src/app/pedidos/pedido-item-drawer/pedido-item-header/pedido-item-header.component.ts
+++ src/app/pedidos/pedido-item-drawer/pedido-item-header/pedido-item-header.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pedido-item-header',
   templateUrl: './pedido-item-header.component.html',
   styleUrls: ['./pedido-item-header.component.scss'],
 })
-export class PedidoItemDetalhesHeaderComponent {}
+export class PedidoItemDetalhesHeaderComponent {
+  @Input() item!: PedidoVendaItem;
+}
