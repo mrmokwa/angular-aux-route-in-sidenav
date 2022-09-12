795d6b47705d7f80fdc7275756d15dc331579f03
Matheus Ravanello Mokwa <matheus@tds.com.br>
2022-09-12 16:18:39 -0300

    remover servico do handler

--- src/app/pedidos/modules/pedido-item-drawer/pages/pedido-item-delete/pedido-item-delete.component.ts
+++ src/app/pedidos/modules/pedido-item-drawer/pages/pedido-item-delete/pedido-item-delete.component.ts
@@ -1,11 +1,10 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { combineLatest, delay, finalize, first, map, Observable } from 'rxjs';
 import { PedidosInfoService } from 'src/app/pedidos/pages/pedido-info/pedido-info.service';
 import { PedidoItemDrawerService } from '../../pedido-item-drawer.service';
-import { PedidoItemHandlerService } from '../pedido-item-handler/pedido-item-handler.service';
 
 @Component({
   selector: 'app-pedido-item-delete',
@@ -13,17 +12,16 @@ import { PedidoItemHandlerService } from '../pedido-item-handler/pedido-item-han
   styleUrls: ['./pedido-item-delete.component.scss'],
   host: { class: 'flex-container' },
 })
-export class PedidoItemDeleteComponent implements OnInit {
+export class PedidoItemDeleteComponent {
   constructor(
     private store: PedidosInfoService,
     private pidService: PedidoItemDrawerService,
-    private handler: PedidoItemHandlerService,
     private router: Router,
-    private snackbar: MatSnackBar
+    private snackbar: MatSnackBar,
+    private route: ActivatedRoute
   ) {}
 
-  ngOnInit(): void {}
-
+  seq$ = this.route.params.pipe(map((params) => +params['seq']));
   item$ = this.pidService.item$;
   erro = '';
 
@@ -47,7 +45,7 @@ export class PedidoItemDeleteComponent implements OnInit {
   }
 
   retornoPedidoMock(): Observable<PedidoDetalhado> {
-    return combineLatest([this.store.pedido$, this.handler.seq$]).pipe(
+    return combineLatest([this.store.pedido$, this.seq$]).pipe(
       delay(1000),
       map(([pedido, seq]) => ({
         ...pedido,
--- src/app/pedidos/modules/pedido-item-drawer/pages/pedido-item-handler/pedido-item-handler.component.ts
+++ src/app/pedidos/modules/pedido-item-drawer/pages/pedido-item-handler/pedido-item-handler.component.ts
@@ -1,32 +1,46 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest, filter, map, Subscription } from 'rxjs';
 import { PedidoItemDrawerService } from '../../pedido-item-drawer.service';
-import { PedidoItemHandlerService } from './pedido-item-handler.service';
 
 @Component({
   selector: 'app-pedido-item-handler',
   templateUrl: './pedido-item-handler.component.html',
   styleUrls: ['./pedido-item-handler.component.scss'],
-  providers: [PedidoItemDrawerService, PedidoItemHandlerService],
+  providers: [PedidoItemDrawerService],
   host: { class: 'flex-container' },
 })
 export class PedidoItemHandlerComponent implements OnInit, OnDestroy {
-  constructor(
-    private store: PedidoItemDrawerService,
-    private handler: PedidoItemHandlerService
-  ) {}
-
   subscription = new Subscription();
-
   loading$ = this.store.loading$;
 
+  constructor(
+    private store: PedidoItemDrawerService,
+    private route: ActivatedRoute
+  ) {}
+
   ngOnInit(): void {
-    const item$ = this.handler.itemDaRota$;
-    const subs = item$.subscribe((data) => this.store.setStore(data));
+    const subs = this.getItemDaRota().subscribe((data) =>
+      this.store.setStore(data)
+    );
     this.subscription.add(subs);
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+
+  getItemDaRota() {
+    const seq$ = this.route.params.pipe(map((params) => +params['seq']));
+
+    const itensPedido$ = this.route.data.pipe(
+      map((data) => data['pedido'] as PedidoDetalhado),
+      map((pedido) => pedido.itens)
+    );
+
+    return combineLatest([seq$, itensPedido$]).pipe(
+      map(([seq, itens]) => itens.find((x) => x.sequencia === seq)),
+      filter(Boolean)
+    );
   }
 }
