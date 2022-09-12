7ca6c1542c2c826b60422ab94272e7a5c0abfdfa
Matheus Ravanello Mokwa <matheus@tds.com.br>
2022-09-12 08:05:17 -0300

    remover interface

--- src/app/pedidos/modules/pedido-item-drawer/components/pedido-item-menu/pedido-item-menu.component.ts
+++ src/app/pedidos/modules/pedido-item-drawer/components/pedido-item-menu/pedido-item-menu.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pedido-item-menu',
   templateUrl: './pedido-item-menu.component.html',
   styleUrls: ['./pedido-item-menu.component.scss'],
 })
-export class PedidoItemMenuComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class PedidoItemMenuComponent {}
