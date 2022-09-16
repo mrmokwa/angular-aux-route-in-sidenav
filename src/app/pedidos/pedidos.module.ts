ce8cbedd73e107c93e55851d1b6d31e608d5c406
Matheus Ravanello Mokwa <matheus@tds.com.br>
2022-09-16 11:34:04 -0300

    update pipe import list

--- src/app/pedidos/pedidos.module.ts
+++ src/app/pedidos/pedidos.module.ts
@@ -1,22 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {
-  PedItemSituacaoClassPipe,
-  PedItemSituacaoPipe,
-} from '../pipes/ped-item-situacao.pipe';
+import { SharedModule } from '../shared/shared.module';
+import { PedItemSituacaoPipes } from '../pipes/ped-item-situacao.pipe';
 import { PedidosRoutingModule } from './pedidos.routing';
 import { PedidoContainerComponent } from './pages/pedido-container/pedido-container.component';
 import { PedidoInfoComponent } from './pages/pedido-info/pedido-info.component';
-import { SharedModule } from '../shared/shared.module';
 import { PedidoItemListaComponent } from './components/pedido-item-lista/pedido-item-lista.component';
 import { ItemConfigPipe } from '../pipes/item-config.pipe';
 import { PedidoDrawerComponent } from './components/pedido-drawer/pedido-drawer.component';
 import { PedidoItemDrawerModule } from './modules/pedido-item-drawer/pedido-item-drawer.module';
 import { PedidoNotaDrawerModule } from './modules/pedido-nota-drawer/pedido-nota-drawer.module';
 import { PedidoToolbarComponent } from './components/pedido-toolbar/pedido-toolbar.component';
-
-const PIPES = [ItemConfigPipe, PedItemSituacaoPipe, PedItemSituacaoClassPipe];
 
 @NgModule({
   declarations: [
@@ -30,7 +25,8 @@ const PIPES = [ItemConfigPipe, PedItemSituacaoPipe, PedItemSituacaoClassPipe];
     CommonModule,
     PedidosRoutingModule,
     SharedModule,
-    PIPES,
+    ItemConfigPipe,
+    PedItemSituacaoPipes,
     PedidoItemDrawerModule,
     PedidoNotaDrawerModule,
   ],
