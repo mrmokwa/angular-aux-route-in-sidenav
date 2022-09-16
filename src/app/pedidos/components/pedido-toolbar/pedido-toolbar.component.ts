c0bef9ddd748c7243b588b7f309e61d2bf70ddae
Matheus Ravanello Mokwa <matheus@tds.com.br>
2022-09-16 11:03:17 -0300

    move toolbar logic to its own component

---
+++ src/app/pedidos/components/pedido-toolbar/pedido-toolbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pedido-toolbar',
+  templateUrl: './pedido-toolbar.component.html',
+  styleUrls: ['./pedido-toolbar.component.scss'],
+})
+export class PedidoToolbarComponent {}
--- src/app/pedidos/pedidos.module.ts
+++ src/app/pedidos/pedidos.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {
+  PedItemSituacaoClassPipe,
+  PedItemSituacaoPipe,
+} from '../pipes/ped-item-situacao.pipe';
 import { PedidosRoutingModule } from './pedidos.routing';
 import { PedidoContainerComponent } from './pages/pedido-container/pedido-container.component';
 import { PedidoInfoComponent } from './pages/pedido-info/pedido-info.component';
 import { SharedModule } from '../shared/shared.module';
 import { PedidoItemListaComponent } from './components/pedido-item-lista/pedido-item-lista.component';
 import { ItemConfigPipe } from '../pipes/item-config.pipe';
-import {
-  PedItemSituacaoClassPipe,
-  PedItemSituacaoPipe,
-} from '../pipes/ped-item-situacao.pipe';
-
 import { PedidoDrawerComponent } from './components/pedido-drawer/pedido-drawer.component';
-
 import { PedidoItemDrawerModule } from './modules/pedido-item-drawer/pedido-item-drawer.module';
 import { PedidoNotaDrawerModule } from './modules/pedido-nota-drawer/pedido-nota-drawer.module';
+import { PedidoToolbarComponent } from './components/pedido-toolbar/pedido-toolbar.component';
 
 const PIPES = [ItemConfigPipe, PedItemSituacaoPipe, PedItemSituacaoClassPipe];
 
@@ -25,6 +24,7 @@ const PIPES = [ItemConfigPipe, PedItemSituacaoPipe, PedItemSituacaoClassPipe];
     PedidoInfoComponent,
     PedidoItemListaComponent,
     PedidoDrawerComponent,
+    PedidoToolbarComponent,
   ],
   imports: [
     CommonModule,
