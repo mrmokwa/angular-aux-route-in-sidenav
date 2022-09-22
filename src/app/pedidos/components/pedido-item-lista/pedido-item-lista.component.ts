49da3a909cdedfc0486fea4baf769a11408462e8
Matheus Ravanello Mokwa <matheus@tds.com.br>
2022-09-22 14:20:36 -0300

    remove interface and constructor

--- src/app/pedidos/components/pedido-item-lista/pedido-item-lista.component.ts
+++ src/app/pedidos/components/pedido-item-lista/pedido-item-lista.component.ts
@@ -1,14 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pedido-item-lista',
   templateUrl: './pedido-item-lista.component.html',
   styleUrls: ['./pedido-item-lista.component.scss'],
 })
-export class PedidoItemListaComponent implements OnInit {
+export class PedidoItemListaComponent {
   @Input() itens: PedidoVendaItem[] = [];
-
-  constructor() {}
-
-  ngOnInit(): void {}
 }
