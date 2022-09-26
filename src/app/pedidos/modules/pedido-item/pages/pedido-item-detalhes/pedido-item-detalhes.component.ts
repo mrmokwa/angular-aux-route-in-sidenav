42b9f2c14730628fd81bd86195457bbc54783dc2
Matheus Ravanello Mokwa <matheus@tds.com.br>
2022-09-26 17:32:26 -0300

    fix loading state after create a new item

--- src/app/pedidos/modules/pedido-item/pages/pedido-item-detalhes/pedido-item-detalhes.component.ts
+++ src/app/pedidos/modules/pedido-item/pages/pedido-item-detalhes/pedido-item-detalhes.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { combineLatest, Subscription } from 'rxjs';
 import { PedidoStoreService } from 'src/app/pedidos/pages/pedido-store/pedido-store.service';
 import { PedidoItemService } from '../../pedido-item.service';
 
@@ -7,7 +8,7 @@ import { PedidoItemService } from '../../pedido-item.service';
   templateUrl: './pedido-item-detalhes.component.html',
   styleUrls: ['./pedido-item-detalhes.component.scss'],
 })
-export class PedidoItemDetalhesComponent {
+export class PedidoItemDetalhesComponent implements OnInit, OnDestroy {
   constructor(
     private itemStore: PedidoItemService,
     private pedidoStore: PedidoStoreService
@@ -15,4 +16,21 @@ export class PedidoItemDetalhesComponent {
 
   pedido$ = this.pedidoStore.pedido$;
   item$ = this.itemStore.item$;
+  subscriptions = new Subscription();
+
+  ngOnInit() {
+    setTimeout(() => this.handleLoadingStateAposInclusaoItem());
+  }
+
+  handleLoadingStateAposInclusaoItem() {
+    const itemSubs = this.item$.subscribe((item) =>
+      this.itemStore.setLoading(!item)
+    );
+
+    this.subscriptions.add(itemSubs);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 }
