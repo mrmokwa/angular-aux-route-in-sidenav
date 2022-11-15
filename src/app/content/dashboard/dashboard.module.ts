ea0962b949f93bfd098a05915c9ae554b5960323
Matheus Ravanello Mokwa <matheus.mokwa@gmail.com>
2022-11-15 15:37:44 -0300

    facilitar navegacao entre pedidos

--- src/app/content/dashboard/dashboard.module.ts
+++ src/app/content/dashboard/dashboard.module.ts
@@ -4,10 +4,9 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard.routing';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MainPageComponent } from './pages/main-page/main-page.component';
-import { OpcoesPedidosComponent } from './components/opcoes-pedidos/opcoes-pedidos.component';
 
 @NgModule({
-  declarations: [MainPageComponent, OpcoesPedidosComponent],
+  declarations: [MainPageComponent],
   imports: [CommonModule, DashboardRoutingModule, SharedModule],
 })
 export class DashboardModule {}
