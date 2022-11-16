ab294ca91ed3e578f877e7b0393a47ceff9cde28
Matheus Ravanello Mokwa <matheus@tds.com.br>
2022-11-16 08:28:47 -0300

    display error msg

--- src/app/content/pedido/pages/pedido-store/pedido-store.resolver.ts
+++ src/app/content/pedido/pages/pedido-store/pedido-store.resolver.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
-import { catchError, EMPTY, finalize, of } from 'rxjs';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { catchError, finalize, of } from 'rxjs';
 import { GlobalLoaderService } from 'src/app/core/services/global-loader.service';
+import { NotificationService } from 'src/app/core/services/notification.service';
 import { PedidosService } from '../../pedidos.service';
+
+const MSG_ERRO = 'Não foi possivel carregar o pedido de venda';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +14,7 @@ export class PedidoStoreResolver implements Resolve<PedidoDetalhado | null> {
   constructor(
     private apiService: PedidosService,
     private loadingService: GlobalLoaderService,
-    private router: Router
+    private notificationService: NotificationService
   ) {}
 
   resolve(route: ActivatedRouteSnapshot) {
@@ -19,7 +22,10 @@ export class PedidoStoreResolver implements Resolve<PedidoDetalhado | null> {
 
     return this.apiService.getById(+route.params['id']).pipe(
       finalize(() => this.loadingService.setState(false)),
-      catchError(() => of(null))
+      catchError(() => {
+        this.notificationService.error(MSG_ERRO);
+        return of(null);
+      })
     );
   }
 }
